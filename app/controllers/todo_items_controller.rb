11965b07bb0f67bd92d1480de61d39fe0bb97e10
Kevin Staiger <kstag07@yahoo.com>
2014-03-27 14:16:32 -0700

    mark item as complete

--- app/controllers/todo_items_controller.rb
+++ app/controllers/todo_items_controller.rb
@@ -4,6 +4,12 @@ class TodoItemsController < ApplicationController
   def index
   end
     
+    def complete
+        @todo_item = @todo_list.todo_items.find(params[:id])
+        @todo_item.update_attribute(:completed_at, Time.now)
+        redirect_to todo_list_todo_items_path, notice: "Todo item marked as complete."
+        end
+    
     def new
         @todo_item = @todo_list.todo_items.new
         end
